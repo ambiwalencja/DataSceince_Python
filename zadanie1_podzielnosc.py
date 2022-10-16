c9ce09123f5991ab18f539df79a30ba6808c21dd
ambiwalencja <helena.a.sokolowska@gmail.com>
2022-10-16 23:10:53 +0200

    pierwsze zadanie - podzielnosc

---
+++ zadanie1_podzielnosc.py
@@ -0,0 +1,15 @@
+# import sys
+
+def check_if_divisible(num_1, num_2):
+    if num_1 % num_2 == 0:
+        divisible = 'divisible'
+    else:
+        divisible = 'not divisible'
+    return divisible
+
+# wywołanie
+
+print('Give two numbers to check if the first one is divisible by the second one.')
+number_1 = int(input('Write the first number:'))
+number_2 = int(input('Now write the second one:'))
+print(check_if_divisible(number_1, number_2))
