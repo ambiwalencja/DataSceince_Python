d120dbb99124937490fe6c3ec572499c2865ad0d
ambiwalencja <helena.a.sokolowska@gmail.com>
2022-10-21 14:16:45 +0200

    corrections

--- task2_prime.py
+++ task2_prime.py
@@ -2,25 +2,19 @@
 
 def print_primes(number):
     prime_numbers_list = []
-    if number == 1:
+    if number == 1 or number == 2:
         return prime_numbers_list
     else:
-        prime_numbers_list.append(1)
-        if number == 2:
-            return prime_numbers_list
-        else:
-            temporary_list = []  # lista przetrzymująca wszystkie liczby mniejsze od aktualnie obrabiajem
-            for i in range(2, number):  # bierzemy po kolei każdą liczbę mniejszą od number
-                temporary_list.append(i)  # do listy pomocniczej dodajemy bieżącą liczbę, zaczynając od 2
-                for j in temporary_list:
-                    if (j != i) and (i % j == 0):  # jeśli i podzielne przez j to następne i
-                        break
-                    elif j == i:   # jak doszliśmy do i = j to dodajemy i do listy
-                        prime_numbers_list.append(i)
-                        continue
-                    else:   # dopóki nie jest równe i nie jest podzielne to następne j
-                        continue
-        return prime_numbers_list
+        temporary_list = []  # lista przetrzymująca wszystkie liczby mniejsze od aktualnie obrabiajem
+        for i in range(2, number):  # bierzemy po kolei każdą liczbę mniejszą od number
+            temporary_list.append(i)  # do listy pomocniczej dodajemy bieżącą liczbę, zaczynając od 2
+            for j in temporary_list:
+                if (j != i) and (i % j == 0):  # jeśli i podzielne przez j to następne i
+                    break
+                elif j == i:   # jak doszliśmy do i = j to dodajemy i do listy
+                    prime_numbers_list.append(i)
+                    continue
+    return prime_numbers_list
 
 
 # execution
