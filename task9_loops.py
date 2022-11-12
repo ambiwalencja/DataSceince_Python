0aa5f1352d12d472406f413a03027f100f9b3e25
ambiwalencja <helena.a.sokolowska@gmail.com>
2022-11-12 18:46:22 +0100

    initial commit

---
+++ task9_loops.py
@@ -0,0 +1,28 @@
+# Używając zagnieżdżonych pętli, stwórz funkcję, która wydrukuje następujący wzór:
+# *
+# * $
+# * $ *
+# * $ * $
+# * $ * $ *
+# * $ * $
+# * $ *
+# * $
+# *
+
+def print_pattern(n):
+    for i in range(1,n,1):
+        for j in range(i):
+            if j % 2 == 0:
+                print('*', end=' ')
+            if j % 2 == 1:
+                print('$', end=' ')
+        print('',end='\n')
+    for i in range(n,0,-1):
+        for j in range(i):
+            if j % 2 == 0:
+                print('*', end=' ')
+            if j % 2 == 1:
+                print('$', end=' ')
+        print('',end='\n')
+
+print_pattern(6)
