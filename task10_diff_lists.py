d092d688703943fd1f895323614eb679a0d688c2
ambiwalencja <helena.a.sokolowska@gmail.com>
2022-11-12 11:33:45 +0100

    added exception handling

--- task10_diff_lists.py
+++ task10_diff_lists.py
@@ -2,12 +2,21 @@
 # i nie występujących w drugiej liście, posortowana rosnąco względem ilości wystąpień w pierwszej liście.
 
 def compare_lists(list1, list2):
-    diff_list = []
-    for digit in list1:
-        if digit not in list2:  # czy tak można???
-            diff_list.append(digit)
-    diff_list = sorted(diff_list, key = diff_list.count)
-    return diff_list
+    try:
+        for element in list1:
+            int(element)
+        for element in list2:
+            int(element)
+    except ValueError:
+        print("Wystąpił błąd. Spróbuj jeszcze raz, upewnij się, że lista zawiera tylko cyfry.")
+    else:
+        diff_list = []
+        for digit in list1:
+            if digit not in list2:
+                diff_list.append(digit)
+        diff_list = sorted(diff_list, key=diff_list.count)
+        return diff_list
+
 
 list_1 = [1,1,2,2,2,3,4,5,9,9,9,9]
 list_2 = [3,4,5,6,7]
