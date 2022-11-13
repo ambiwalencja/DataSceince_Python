f39d1e476079f2ca05d84c6511321bd3fadc1200
ambiwalencja <helena.a.sokolowska@gmail.com>
2022-11-13 09:54:37 +0100

    deleted comments

--- task14_power.py
+++ task14_power.py
@@ -1,17 +1,10 @@
 # Napisz (przy pomocy rekurencji) funkcję realizującą potęgowanie.
 # Funkcja przyjmuje dwie liczby całkowite, będące podstawą i wykładnikiem potęgi.
 
-# def power(n, m):
-#     result = 1
-#     for i in range(m):
-#         result = result * n
-#     return result
-
-# sama wpadłam tylko na rozwiązanie bez-rekurekcyjne, poniższe rozwiązanie zgapione z sieci
-def power2(n, m):
+def power(n, m):
     if m == 0:
         return 1
-    return n*power2(n,m-1)
+    return n*power(n,m-1)
 
-print(power2(2,8))
+print(power(2,8))
 
