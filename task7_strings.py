13572ba60e3bd52250c7c1e276d82e2fd6fbabc5
ambiwalencja <helena.a.sokolowska@gmail.com>
2022-11-25 22:27:39 +0100

    added another solution with list comprehension

--- task7_strings.py
+++ task7_strings.py
@@ -9,5 +9,13 @@ def count_letters_and_numbers(my_string):
             letters += 1
     return digits, letters
 
-result = count_letters_and_numbers('kjhjk4563j*&*^')
+# podpowiedź - użyć list comprehension
+# https://stackoverflow.com/questions/35758968/count-in-python-list-comprehension
+
+def count_letters_and_numbers_2(my_string):
+    digits = len([1 for char in my_string if char.isdigit()])
+    letters = len([1 for char in my_string if char.isalpha()])
+    return digits, letters
+
+result = count_letters_and_numbers_2('kjhjk4563j*&*^')
 print(f'Number of digits: {result[0]}, number of letters: {result[1]}')
